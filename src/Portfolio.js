710047c644a57362cfef299202d514a0beff6721
Ben Yarrow <107545312+BenYarrow@users.noreply.github.com>
2023-04-28 12:07:11 +0100

    Added basic form template to contact us page

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,6 +3,14 @@ import React from "react";
 const portfolioWork = [
     {
         videoSrc: '',
+        title: 'Lux Le Morne',
+        location: 'Le Morne, Mauritius',
+        description: `Blessed with the best sunset of the island, life’s a beach at LUX* Le Morne. The chic and serene boutique resort at the foot of Le Morne mountain calls all lovers of mindful luxury. The postcard beach barely needs an introduction. As for the hotel, it's a gem that not only embodies the spirit of island living- it elevates it.`,
+        visitSite: 'https://www.luxresorts.com/en/mauritius/hotel/luxlemorne',
+        map: '',
+    },
+    {
+        videoSrc: '',
         title: 'Swiss Inn',
         location: 'Dahab, Egypt',
         description: 'Hidden in the vast desert and majestic mountains of South Sinai, lies the charming Swiss Inn Resort on the shores of the Red Sea, on the Gulf of Aqaba The friendly hospitality, excellent service and genuine warmth help us to create an atmosphere that keeps our guests coming back again and again.',
@@ -41,14 +49,6 @@ const portfolioWork = [
         visitSite: 'https://safirhotels.com/en/hotel/dahab',
         map: '',
     },
-    {
-        videoSrc: '',
-        title: 'Cosmos',
-        location: 'Vassiliki, Greece',
-        description: 'Located at the windward end of the bay which is recognized for providing optimum conditions and longest sail time.',
-        visitSite: 'https://safirhotels.com/en/hotel/dahab',
-        map: '',
-    }
 ];
 
 const PortfolioCard = ({work: {videoSrc, title, location, description, visitSite, map   }}) => {
