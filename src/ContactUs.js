726184f9f988e0e54a1afb757c0aa77e3e3ce806
Ben Yarrow <107545312+BenYarrow@users.noreply.github.com>
2023-05-19 18:09:58 +0100

    Updated contact us form using emailjs to send and recieve emails to our email address. CSS needs adding. The email from function does not work with personal accounts on emailjs, add a note in the message input to include emails before submitting

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,55 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './Css/Contact-Us.css';
 
+
 const ContactUs = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_5w3yz3d', 'template_2vjlsqn', form.current, 'iZx_RyLr0vI8W72Xu')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
-    <div id='contact-us' className='w-full bg-standard flex justify-center'>
-      <div className='flex items-center'>
-      <form
-      action=''
-      onSubmit=''
-      method=''
-      target=''
-    >
-      <div className="mb-4 pt-0">
-        <input
-          type="text"
-          placeholder="Your name"
-          name="name"
-          className="px-4 py-4 text-gray-600  bg-white rounded-lg text-sm w-full"
-          required
-        />
-      </div>
-      <div className="mb-4 pt-0">
-        <input
-          type="email"
-          placeholder="Your email address"
-          name="email"
-          className="px-4 py-4 text-gray-600 bg-white rounded-lg text-sm w-full"
-          required
-        />
-      </div>
-      <div className="mb-4 pt-0">
-        <textarea
-          placeholder="Your message"
-          name="message"
-          className="px-4 py-4 placeholder-gray-400 text-gray-600 bg-white rounded-lg text-sm w-full"
-          required
-        />
-      </div>
-      <div className="mb-4 pt-0 ">
-        <button
-          className="bg-slate-600 text-slate-200 active:bg-slate-400 active:text-slate-600 font-bold uppercase text-sm px-8 py-4 rounded-lg ease-linear transition-all duration-150"
-          type="submit"
-        >
-          Send a message
-        </button>
-      </div>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
     </form>
-    
-      </div>
-    </div>
-  )
-}
+  );
+};
+
 
 export default ContactUs;
+
+
