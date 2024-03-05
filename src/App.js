355361ecaab2fc5f1e435f15fe18bf6b9cee911e
Ben Yarrow <107545312+BenYarrow@users.noreply.github.com>
2024-03-05 20:59:08 +0000

    Looking to add vercel analytics to the site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ScrollToTop from './components/ScrollToTop';
@@ -40,6 +41,7 @@ function App() {
             </Routes>
           <Footer />  
         </Router>
+        <Analytics />
       </div>
   );
 }
