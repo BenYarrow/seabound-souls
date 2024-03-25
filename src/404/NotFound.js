7e96612aaf7dec957cd6e618e15b5df6973d7667
Ben Yarrow <107545312+BenYarrow@users.noreply.github.com>
2024-03-25 22:08:57 +0000

    Update 404 page

--- src/404/NotFound.js
+++ src/404/NotFound.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import BlockWrapper from '../components/BlockWrapper'
 import Text from '../components/Text'
-import Title from '../components/Title'
 
 const NotFound = () => {
   return (
     <BlockWrapper>
-        <Title title="Page Not Fund" h1/>
-        <Text subTitle="Unfortunately the page cannot be found right now" content={["Please check the url and try again"]}/>
+        <div className='flex flex-col items-center gap-y-8'>
+            <div>
+                <h1 className="text-3xl flex flex-col items-center gap-y-4 py-4">
+                    <span className="text-6xl text-center">
+                        404
+                    </span>
+                    <span className="block text-center uppercase">
+                        Page not found
+                    </span>
+                </h1>
+                <Text centredText 
+                    content={[
+                        "The page you are loking for might have been removed, had its name changed or is temporarily unavailable.", 
+                        () => <p>If you're not sure how you got here, please head back to the <Link to="/">homepage</Link> to try again</p>,
+                        () => <p>If this issue persists, please navigate to our <Link to="/contact-us">contact page</Link> and let us know what youre experiencing, and we will look to resolve the issue as soon as possible.</p>
+                ]}/>
+            </div>
+        </div>
     </BlockWrapper>
   )
 }
