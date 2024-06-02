e5bb003f2525f8ef91cfa9b66430b2b062cb9ab6
Ben Yarrow <107545312+BenYarrow@users.noreply.github.com>
2024-06-02 20:00:36 +0100

    Build split image text, remove unused code and rename files

--- src/nav-bar-pages/Homepage.js
+++ src/nav-bar-pages/Homepage.js
@@ -3,8 +3,6 @@ import { spotGuideLinks } from "../Data/spot-guide-links";
 import { blogLinks } from "../Data/blog-links";
 import SwiperHeroSlider from "../components/SwiperHeroSlider";
 import SiteHelmet from "../components/SiteHelmet";
-import BlockWrapper from "../components/BlockWrapper";
-import Text from "../components/Text";
 import ListBlogs from "../components/ListBlogs";
 import SplitImageText from "../components/SplitImageText";
 
@@ -68,58 +66,6 @@ function Homepage() {
         images={heroImages}
         title="Memories are made where the wind meets the waves"
       />
-
-      {/* <BlockWrapper padded>
-        <div
-          id="content"
-          className="grid grid-cols lg:grid-cols-2 xl:grid-cols-9 gap-8"
-        >
-          <div className="xl:col-span-6 flex lg:justify-center flex-col gap-6 lg:gap-12">
-            <Text
-              title="Get to know us"
-              content={[
-                "Hi! We are Rachel and Ben. We're two adventure seekers, travelling the world wherever the wind takes us.",
-                "Follow our journey to discover the most incredible windsurfing destinations around the globe.",
-                "This isn't just a travel blog; it's a unique blend of travel, adrenaline and first hand experiences. Whether you're an experienced windurfer seeking the next ultimate spot or a curious traveller ready to climb onto a windsurf board, our blog is here to inspire, inform and fuel your passion for this incredible water sport.",
-                () => (
-                  <span className="gap-1 text-black">
-                    <a
-                      href="https://www.instagram.com/seabound.souls"
-                      target="_blank"
-                      rel="nofollow external noopener noreferrer"
-                      className="whitespace-nowrap pr-1.5"
-                    >
-                      Join us
-                    </a>
-                    as we chase the winds, discover new horizons and travel to
-                    the very best windsurf destinations.
-                  </span>
-                ),
-              ]}
-            />
-          </div>
-
-          <picture className="xl:col-span-3">
-            <source
-              media="(max-width: 640px)"
-              srcSet="/images/mastheads/about-us/fuerteventura-beach-tablet.jpg"
-            />
-            <source
-              media="(min-width: 641px) and (max-width: 1024px)"
-              srcSet="/images/mastheads/about-us/fuerteventura-beach-mobile.jpg"
-            />
-            <img
-              src="/images/mastheads/about-us/fuerteventura-beach-mobile.jpg"
-              width="328"
-              height="246"
-              alt="Ben and Rachel on a beach in Fuerteventura"
-              className="w-full object-left md:object-center object-cover "
-              loading="lazy"
-              crossOrigin="true"
-            />
-          </picture>
-        </div>
-      </BlockWrapper> */}
 
       <SplitImageText
         image={introImage}
