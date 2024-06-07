a403ca45a4c2b1a895bdf31039b6708d8b46b6c5
Ben Yarrow <107545312+BenYarrow@users.noreply.github.com>
2024-06-07 13:43:25 +0100

    REmove animation

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,5 +1,4 @@
-import React, {useRef, useEffect} from 'react';
-import { motion, useAnimation, useInView } from "framer-motion";
+import React from 'react';
 import { checkContentFormat, defaultProseClasses } from '../helpers/functions';
 
 const Text = ({ 
@@ -12,52 +11,27 @@ const Text = ({
     lineClamp,
 }) => {
   
-
-  const textRef = useRef(null)
-  const isTextInView = useInView(textRef, {once: true})
-    
-  const mainControls = useAnimation()
-
-  useEffect(() => {
-    if (isTextInView) {
-      mainControls.start('visible')
-    }
-  }, [isTextInView,  mainControls])
-  
   const blockContent = checkContentFormat(content) 
 
   return (
-        <motion.div ref={textRef}  
-          variants={{
-            hidden: {opacity: 0, y: 75},
-            visible: {opacity: 1, y: 0},
-          }}
-          initial= 'hidden'
-          animate={mainControls}
-          transition={{
-            duration: 0.5,
-            delay: 0.5
-          }}
-        >
-          <div className={defaultProseClasses({ invert, centredText, centreHeading })}>
+      <div className={defaultProseClasses({ invert, centredText, centreHeading })}>
 
-            {title && <h2>{title}</h2>}
+        {title && <h2>{title}</h2>}
 
-            {subtitle && <h3>{subtitle}</h3>}
+        {subtitle && <h3>{subtitle}</h3>}
 
-            {blockContent && blockContent.map((item, index) => (
-                <div key={index}>
-                  {typeof item === 'string' || Array.isArray(item) ? (
-                    <p className={lineClamp ? lineClamp : ''}>{item}</p>
-                  ) : typeof item === 'function' ? (
-                    item()
-                  ) : (
-                    null
-                  )}
-                </div>
-              ))}
-          </div>
-      </motion.div>
+        {blockContent && blockContent.map((item, index) => (
+            <div key={index}>
+              {typeof item === 'string' || Array.isArray(item) ? (
+                <p className={lineClamp ? lineClamp : ''}>{item}</p>
+              ) : typeof item === 'function' ? (
+                item()
+              ) : (
+                null
+              )}
+            </div>
+          ))}
+      </div>
   );
 };
 
